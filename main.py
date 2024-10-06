b09f2f1979a30912395f195565d7e9c2bfe61e49
Udit <uditrawat1905@gmail.com>
2024-10-06 20:45:09 +0530

    updated components for the stage 2 [TESTED]

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.mlpro.pipeline.data_ingestion_pipe import DataIngestionTrainingPipeline
+from src.mlpro.pipeline.data_validation_pipe import DataValidationTrainingPipeline
 from src.mlpro import logger
 
 
@@ -6,6 +7,16 @@ StageName = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>>>> stage {StageName} Initiated <<<<<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(
+        f">>>>>>>> stage {StageName} Commenced <<<<<<<<<\n\nx=================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+StageName = "Data Validation Stage"
+try:
+    logger.info(f">>>>>>>> stage {StageName} Initiated <<<<<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(
         f">>>>>>>> stage {StageName} Commenced <<<<<<<<<\n\nx=================x")
--- src/mlpro/config/configuration.py
+++ src/mlpro/config/configuration.py
@@ -1,7 +1,7 @@
 from src.mlpro.entity import config_entity
 from src.mlpro.constants import *
 from src.mlpro.utils.common import read_yaml, create_directories
-from src.mlpro.entity.config_entity import DataIngestionConfig
+from src.mlpro.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
 
 class ConfigurationManager:
@@ -30,3 +30,14 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+        create_directories([config.root_dir])
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            all_schema=schema,
+            unzip_data_dir=config.unzip_data_dir)
+        return data_validation_config
