1ad7fc489220ed94f7b37da4ab40321bfe134340
Udit <uditrawat1905@gmail.com>
2024-10-07 18:34:49 +0530

    updated components for stage3 [TESTED]

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlpro.pipeline.data_ingestion_pipe import DataIngestionTrainingPipeline
 from src.mlpro.pipeline.data_validation_pipe import DataValidationTrainingPipeline
+from src.mlpro.pipeline.data_transformation_pipe import DataTransformationTrainingPipeline
 from src.mlpro import logger
 
 
@@ -17,6 +18,16 @@ StageName = "Data Validation Stage"
 try:
     logger.info(f">>>>>>>> stage {StageName} Initiated <<<<<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(
+        f">>>>>>>> stage {StageName} Commenced <<<<<<<<<\n\nx=================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+StageName = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>>>> stage {StageName} Initiated <<<<<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(
         f">>>>>>>> stage {StageName} Commenced <<<<<<<<<\n\nx=================x")
--- src/mlpro/config/configuration.py
+++ src/mlpro/config/configuration.py
@@ -1,7 +1,7 @@
 from src.mlpro.entity import config_entity
 from src.mlpro.constants import *
 from src.mlpro.utils.common import read_yaml, create_directories
-from src.mlpro.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from src.mlpro.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 
 
 class ConfigurationManager:
@@ -41,3 +41,15 @@ class ConfigurationManager:
             all_schema=schema,
             unzip_data_dir=config.unzip_data_dir)
         return data_validation_config
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
