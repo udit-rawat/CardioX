93c1d1b4c567bc7597814271e586c460c2eb179a
Udit <uditrawat1905@gmail.com>
2024-10-14 00:21:58 +0530

    main file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.mlpro.pipeline.data_ingestion_pipe import DataIngestionTrainingPipeline
 from src.mlpro.pipeline.data_validation_pipe import DataValidationTrainingPipeline
 from src.mlpro.pipeline.data_transformation_pipe import DataTransformationTrainingPipeline
 from src.mlpro.pipeline.model_trainer_pipe import ModelTrainerTrainingPipeline
+from src.mlpro.pipeline.model_eval_pipe import ModelEvaluationTrainingPipeline
 from src.mlpro import logger
 
 
@@ -41,6 +42,15 @@ try:
     data_ingestion = ModelTrainerTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} Commenced <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+STAGE_NAME = "Model evaluation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = ModelEvaluationTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
